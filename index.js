ae872f5fcc9452a9e288f7015ee6427be4ab9c79
Zlata <zshimova@bk.ru>
2024-09-26 19:26:51 +0300

    fetch request works

---
+++ index.js
@@ -0,0 +1,10 @@
+// Получение изображений
+let data = fetch("https://dog.ceo/api/breeds/image/random/20")
+    .then(res => res.json())
+    .then(res => {
+        let objs = res.message;
+        objs.forEach(obj => console.log(obj));
+        // console.log(res) - проверка ответа res
+    })
+    .catch(error => console.log("Ошибка запроса:", error));
+// console.log(data);
