b7b4f65b569b10fa09bcfcf6de847a33dbee3de0
Zlata <zshimova@bk.ru>
2024-09-27 14:22:41 +0300

    fix async await function for getting fetch

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 // Импорт стилей
-import './style.scss';  // Импортируем SCSS файл
-
-
+import './style.scss';
 
 // Переменные
 const btn = document.getElementById('btn');
-
+const imgContainer = document.getElementById('imgContainer');
 
 
 // Получение изображений
-let data = fetch("https://dog.ceo/api/breeds/image/random/20")
-    .then(res => res.json())
-    .then(res => {
-        let objs = res.message;
-        objs.forEach(obj => console.log(obj));
-        // console.log(res) - проверка ответа res
-    })
-    .catch(error => console.log("Ошибка запроса:", error));
-// console.log(data);
+async function getData() {
+    try {
+        let data = await fetch("https://dog.ceo/api/breeds/image/random/20");
+        let data2 = await data.json();
+
+        console.log(data2)
+    } catch {
+        error => console.log("Error", error)
+    } finally {
+        console.log("промис выполнен")
+    }
+};
+getData();
 
 
+// Клик по кнопке для загрузки изображений
+btn.addEventListener("click", loadImg());
+
+// Функция для загрузки
+function loadImg() {
+
+}
