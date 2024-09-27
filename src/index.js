87c29133916940e4aad20c38a75381b797495879
Zlata <zshimova@bk.ru>
2024-09-27 19:51:38 +0300

    fix: fetch request; added: code for html in js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import './style.scss';
 
 // Переменные
 const btn = document.getElementById('btn');
-const imgContainer = document.getElementById('imgContainer');
+const imgContainer = document.querySelector('.img-container');
 
 
 // Получение изображений
 async function getData() {
     try {
-        let data = await fetch("https://dog.ceo/api/breeds/image/random/20");
-        let data2 = await data.json();
-
-        console.log(data2)
-    } catch {
-        error => console.log("Error", error)
+        let response = await fetch("https://dog.ceo/api/breeds/image/random/20");
+        let data = await response.json();
+        // console.log(data);
+        return data.message;
+    } catch (error) {
+        console.log("Error", error)
     } finally {
         console.log("промис выполнен")
     }
@@ -23,9 +23,20 @@ getData();
 
 
 // Клик по кнопке для загрузки изображений
-btn.addEventListener("click", loadImg());
+btn.addEventListener("click", loadImg);
 
-// Функция для загрузки
-function loadImg() {
 
+// Функция встраивания изображений в разметку
+async function loadImg() {
+    const data = await getData();
+    
+    imgContainer.innerHTML = '';
+    data.forEach(url => {
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = "Dog image";
+        img.style.width = '200px';
+        img.style.height = '200px';
+        imgContainer.appendChild(img);
+    });
 }
